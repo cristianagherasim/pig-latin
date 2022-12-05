30536077e74c950d3d09399443aebc4750da8a62
Cristiana <cristiana.gherasim@gmail.com>
2022-12-05 14:32:00 -0500

    completed

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
-function translate(str){
-    let strArr = str.split(' ');
-    let pigLatin = [];
+// function translate(str){
+//     let strArr = str.split(' ');
+//     let pigLatin = [];
   
-    for(let word of strArr){
-      if((/([a-zA-Z])/).test(word)){
-        pigLatin.push(word.substring(1) + word[0] + "ay");
-      }else{
-        pigLatin.push(word);
-      }
-    }
-    return pigLatin.join(" ");
-  }
+//     for(let word of strArr){
+//       if((/([a-zA-Z])/).test(word)){
+//         pigLatin.push(word.substring(1) + word[0] + "ay");
+//       }else{
+//         pigLatin.push(word);
+//       }
+//     }
+//     return pigLatin.join(" ");
+//   }
 
-  module.exports = (translate);
+//   module.exports = (translate);
+
+function translate(rawWord) {
+    word = rawWord.toLowerCase();
+    let vowels = ["a","e","i","o","u"];
+
+    if(vowels.indexOf(word[0]) > -1) {
+        return word + "way";
+    } else {
+        for (var i=0; i < word.length; i++) {
+            if (vowels.includes(word[i])) {
+              let firstConsonants = word.slice(0,i);
+              let restOfWord = word.slice(i, word.length);
+            return restOfWord + firstConsonants + "ay";
+            }
+        }
+    }
+}
+
+module.exports = {translate};
+
--- main.test.js
+++ main.test.js
@@ -1,24 +1,82 @@
 const {expect, test, describe} = require("@jest/globals");
 
-let main = require('./main');
+const main = require('./main');
 
 // words that start with vowels… write tests for several different starting vowels
 
-test('translate to pig latin', () => {
-    
-    let testPiglatin = main('Good Night');
+test('starts with e', () => {
+        let testPiglatin = main.translate('egg');
+        expect(testPiglatin).toBe('eggway');
+   })
 
-    expect(testPiglatin).toBe('oodGay ightNay');
-
-});
+test('starts with o', () => {
+    let testPiglatin = main.translate('orange');
+    expect(testPiglatin).toBe('orangeway');
+})
+                                                                                                                                                                                                                                                                                                                                                                                    
 
 // words that start with vowels with capital letters are converted to lowercase
 
+test('starts with capital Vowel', () => {
+    let testPiglatin = main.translate('Orange');
+    expect(testPiglatin).toBe('orangeway');
+})
+
+test('starts with capital Vowel', () => {
+    let testPiglatin = main.translate('Alaska');
+    expect(testPiglatin).toBe('alaskaway');
+})
+
+test('starts with capital Vowel', () => {
+    let testPiglatin = main.translate('Expecting');
+    expect(testPiglatin).toBe('expectingway');
+})
 
 // words that start with one consonant… write tests for the second letter being several different vowels
+test('starts with consonant…different  vowels', () => {
+    let testPiglatin = main.translate('bee');
+    expect(testPiglatin).toBe('eebay');
+})
 
+test('starts with consonant…different  vowels', () => {
+    let testPiglatin = main.translate('target');
+    expect(testPiglatin).toBe('argettay');
+})
+
+test('starts with consonant…different  vowels', () => {
+    let testPiglatin = main.translate('poser');
+    expect(testPiglatin).toBe('oserpay');
+})
 
 // words that start with two consonants
 
+test('starts with two consonants', () => {
+    let testPiglatin = main.translate('pray');
+    expect(testPiglatin).toBe('aypray');
+})
 
+test('starts with two consonants', () => {
+    let testPiglatin = main.translate('score');
+    expect(testPiglatin).toBe('orescay');
+})
+
+test('starts with two consonants', () => {
+    let testPiglatin = main.translate('growing');
+    expect(testPiglatin).toBe('owinggray');
+})
 // words that start with more consonants
+
+test('starts with more consonants', () => {
+    let testPiglatin = main.translate('throwing');
+    expect(testPiglatin).toBe('owingthray');
+})
+
+test('starts with more consonants', () => {
+    let testPiglatin = main.translate('school');
+    expect(testPiglatin).toBe('oolschay');
+})
+
+test('starts with more consonants', () => {
+    let testPiglatin = main.translate('christmas');
+    expect(testPiglatin).toBe('istmaschray');
+})
